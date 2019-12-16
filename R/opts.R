610909779adaccbcac1c4823b512fe5c0cb9ee78
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2019-12-16 17:12:35 +0100

    updated manual

--- R/opts.R
+++ R/opts.R
@@ -1,36 +1,37 @@
-#' Options for the rock package
+#' Options for the escalc package
 #'
-#' The `rock::opts` object contains three functions to set, get, and reset
-#' options used by the rock package. Use `rock::opts$set` to set options,
-#' `rock::opts$get` to get options, or `rock::opts$reset` to reset specific or
+#' The `escalc::opts` object contains three functions to set, get, and reset
+#' options used by the escalc package. Use `escalc::opts$set` to set options,
+#' `escalc::opts$get` to get options, or `escalc::opts$reset` to reset specific or
 #' all options to their default values.
 #'
-#' It is normally not necessary to get or set `rock` options. The defaults implement
-#' the Reproducible Open Coding Kit (ROCK) standard, and deviating from these defaults
-#' therefore means the processed sources and codes are not compatible and cannot be
-#' processed by other software that implements the ROCK. Still, in some cases this
-#' degree of customization might be desirable.
+#' It is normally not necessary to get or set `escalc` options.
 #'
 #' The following arguments can be passed:
 #'
 #' \describe{
-#'   \item{...}{For `rock::opts$set`, the dots can be used to specify the options
-#'   to set, in the format `option = value`, for example, `utteranceMarker = "\n"`. For
-#'   `rock::opts$reset`, a list of options to be reset can be passed.}
-#'   \item{option}{For `rock::opts$set`, the name of the option to set.}
-#'   \item{default}{For `rock::opts$get`, the default value to return if the
+#'   \item{...}{For `escalc::opts$set`, the dots can be used to specify the options
+#'   to set, in the format `option = value`, for example,
+#'   `EFFECTSIZE_POINTESTIMATE_NAME_IN_DF = "\n"`. For
+#'   `escalc::opts$reset`, a list of options to be reset can be passed.}
+#'   \item{option}{For `escalc::opts$set`, the name of the option to set.}
+#'   \item{default}{For `escalc::opts$get`, the default value to return if the
 #'   option has not been manually specified.}
 #' }
 #'
 #' The following options can be set:
 #'
 #' \describe{
+#' 
 #'   \item{EFFECTSIZE_POINTESTIMATE_NAME_IN_DF}{The name of the column
 #'   with the effect size values.}
+#'   
 #'   \item{EFFECTSIZE_VARIANCE_NAME_IN_DF}{The name of the column
 #'   with the effect size variance.}
+#'   
 #'   \item{EFFECTSIZE_MISSING_MESSAGE_NAME_IN_DF}{The name of the column
 #'   with the missing values.}
+#'   
 #' }
 #'
 #' @aliases opts set get reset
