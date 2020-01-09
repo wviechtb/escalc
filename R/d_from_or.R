5597e4162960dc5262797bea3ac4fefd19ca07e7
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2020-01-09 13:45:12 +0100

    Implemented a bit of 'opts'

--- R/d_from_or.R
+++ R/d_from_or.R
@@ -51,6 +51,6 @@ d_from_or <- function(or, lb, ub, ci=95, dist="normal") {
   v <- ifelse(dist == "logistic", selor^2 * 3 / pi^2, selor^2 * .607^2)
 
   return(stats::setNames(data.frame(d, v),
-                         c(.EFFECTSIZE_POINTESTIMATE_NAME_IN_DF,
-                           .EFFECTSIZE_VARIANCE_NAME_IN_DF)))
+                         c(opts$get("EFFECTSIZE_POINTESTIMATE_NAME_IN_DF"),
+                           opts$get("EFFECTSIZE_VARIANCE_NAME_IN_DF"))))
 }
